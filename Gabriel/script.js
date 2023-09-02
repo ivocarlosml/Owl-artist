761d93694d72212e432f8059fde05b862cd92af1
GabrielxBarreto <gabrieloakbarreto@gmail.com>
2023-09-02 18:23:20 -0300

    .

--- Gabriel/script.js
+++ Gabriel/script.js
@@ -3,7 +3,7 @@ let i = 0;
 
 setInterval(function(){
     let listaDeposts = [
-        "R.jpg","https://wallpapercave.com/wp/wp9376389.jpg","https://images.hdqwalls.com/wallpapers/tokyo-ghoul-anime-4k-o1.jpg","https://th.bing.com/th/id/R.bd25fe33dbd21db91249a661f3e4ecd3?rik=zALwgy84d1Ca%2fg&riu=http%3a%2f%2f1.bp.blogspot.com%2f-L91Zp81c47Q%2fUDV1uQL0iTI%2fAAAAAAAACk4%2fP1UcSbUd6Hw%2fs1600%2fIMG_6892.jpg&ehk=jHMPm4zZVkyRpkPfGwVJ7K7Ex6sBJOrSr6sr9CR2TQM%3d&risl=&pid=ImgRaw&r=0"];
+        "R.jpg","https://wallpapercave.com/wp/wp9376389.jpg","https://th.bing.com/th/id/R.bd25fe33dbd21db91249a661f3e4ecd3?rik=zALwgy84d1Ca%2fg&riu=http%3a%2f%2f1.bp.blogspot.com%2f-L91Zp81c47Q%2fUDV1uQL0iTI%2fAAAAAAAACk4%2fP1UcSbUd6Hw%2fs1600%2fIMG_6892.jpg&ehk=jHMPm4zZVkyRpkPfGwVJ7K7Ex6sBJOrSr6sr9CR2TQM%3d&risl=&pid=ImgRaw&r=0"];
     
     if(i == (listaDeposts.length-1)){
         postsPrincipais.classList.toggle('opacityAnimation');
@@ -16,7 +16,8 @@ setInterval(function(){
         
         postsPrincipais.src= listaDeposts[i] ;
         i++;
+        
     }
 
-},2000);
+},3000);
 
