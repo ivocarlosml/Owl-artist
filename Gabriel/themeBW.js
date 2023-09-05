07d54d8ccc7b14a8572553ef57e67372e334e93f
GabrielxBarreto <gabrieloakbarreto@gmail.com>
2023-09-05 06:49:44 -0300

    consegui sobrepor os icones sobre o grid

--- Gabriel/themeBW.js
+++ Gabriel/themeBW.js
@@ -4,7 +4,7 @@ let header = document.getElementById("header");
 
 function turnColorPage(){
 document.body.style.backgroundColor="#eef6ff";
-document.footer.style.backgroundColor="#eef6ff";
+//document.footer.style.backgroundColor="#eef6ff";
 header.style.backgroundColor="white";
 header.div.style.color="black";
 posterP.style.boxShadow="0 30px 0px rgb(108 107 107)"
