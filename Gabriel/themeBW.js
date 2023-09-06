698bcfe501091c97d1827e06e08bb39a3fea051a
GabrielxBarreto <gabrieloakbarreto@gmail.com>
2023-09-05 23:44:15 -0300

    Importante
    efeito hover com dilatação em todos os elementos
    melhorias do tema claro (não acabado)
    ajustes de algumas paletas de cores

--- Gabriel/themeBW.js
+++ Gabriel/themeBW.js
@@ -1,11 +1,27 @@
 let btn = document.getElementById("btnTurnMode");
 let posterP = document.getElementsByClassName(".imagem-container");
 let header = document.getElementById("header");
-
+let footer = document.getElementsByTagName("footer")[0];
+let control = 0;
 function turnColorPage(){
-document.body.style.backgroundColor="#eef6ff";
-//document.footer.style.backgroundColor="#eef6ff";
-header.style.backgroundColor="white";
-header.div.style.color="black";
-posterP.style.boxShadow="0 30px 0px rgb(108 107 107)"
+    
+    if(control == 0){
+    document.body.style.backgroundColor="#eef6ff";
+    //document.footer.style.backgroundColor="#eef6ff";
+    header.style.backgroundColor="white";
+    footer.style.backgroundColor="white";
+    //header.div.style.color="black";
+    //posterP.style.boxShadow="0 30px 0px rgb(108 107 107)";
+    control = 1;
+    }else if(control == 1){
+    document.body.style.backgroundColor="black";
+    //document.footer.style.backgroundColor="#eef6ff";
+    header.style.backgroundColor="black";
+    footer.style.backgroundColor="black";
+    //header.div.style.color="white";
+    //posterP.style.boxShadow="0 30px 0px rgb(108 107 107)";
+    control = 0;
+    }
+
+
 }
