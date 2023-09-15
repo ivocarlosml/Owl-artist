5c619b5724ad76fc4fd0e5f1918ebbc8ed53bf63
GabrielxBarreto <gabrieloakbarreto@gmail.com>
2023-09-14 22:49:23 -0300

    ajustes da tela de vizualização
    adicionei imagens novas e ajustes da div de img

--- js/icones.js
+++ js/icones.js
@@ -4,20 +4,20 @@ document.addEventListener("DOMContentLoaded", function () {
     var imagem = document.getElementById("coracao");
 
     imagem.addEventListener("click", function () {
-        if (imagem.src.endsWith("coracaoBranco.png")) {
+        if (imagem.src.endsWith("assets/coracaoBranco.png")) {
             imagem.classList.add("hidden"); // Adiciona a classe "hidden" para iniciar a animação
             setTimeout(function () {
-                imagem.src = "coracaoVermelho.png";
+                imagem.src = "assets/coracaoVermelho.png";
                 imagem.alt = "coração vermelho";
                 imagem.classList.remove("hidden"); // Remove a classe "hidden" para revelar a nova imagem
-            }, 1000); // Tempo em milissegundos (0.5s)
+            }, 350); // Tempo em milissegundos (0.5s)
         } else {
             imagem.classList.add("hidden");
             setTimeout(function () {
-                imagem.src = "coracaoBranco.png";
+                imagem.src = "assets/coracaoBranco.png";
                 imagem.alt = "coração branco";
                 imagem.classList.remove("hidden");
-            }, 1000);
+            },350);
         }
     });
 });
